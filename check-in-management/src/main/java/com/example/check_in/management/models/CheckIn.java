33498c41a33e62a57d32aa192656b7569a029071
StivenDelgado <delgadostiven123@gmail.com>
2025-04-03 22:36:52 -0500

    created models microservice

---
+++ check-in-management/src/main/java/com/example/check_in/management/models/CheckIn.java
@@ -0,0 +1,53 @@
+package com.example.check_in.management.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.CascadeType;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "check_in")
+public class CheckIn {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "check_in_id")
+    private Long check_in_id;
+    
+    @Column(name = "user_id")
+    private Long user_id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "check_in_type_id")
+    private CheckInType checkInType;            
+
+    @Column(name = "start_time")
+    private String startTime;
+
+    @Column(name = "end_time")
+    private String endTime;
+
+    @Column(name = "week_day")
+    private String weekDay;
+
+    @Column(name = "daily_hours")
+    private int dailyHours;
+
+    @CreationTimestamp
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime updatedAt;
+
+}
---
+++ check-in-management/src/main/java/com/example/check_in/management/models/CheckInType.java
@@ -0,0 +1,35 @@
+package com.example.check_in.management.models;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "check_in_type")
+public class CheckInType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "check_in_type_id")
+    private Long checkInTypeId;    
+
+    @Column(name = "description")
+    private String description;
+
+    @CreationTimestamp
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime updatedAt;
+}
